ddf72025d0e9a0016eef86bae4924583a00a5404
Tori Pope <toricpope@gmail.com>
2019-03-13 17:11:50 -0500

    logout button

--- screens/Dashboard.js
+++ screens/Dashboard.js
@@ -1,13 +1,27 @@
 import React from 'react'
-import { StyleSheet, Platform, Image, Text, View } from 'react-native';
+import { StyleSheet, Platform, Image, Text, View, Button } from 'react-native';
 import firebase from '../server/config';
 
 export default class Dashboard extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleLogOut = this.handleLogOut.bind(this)
+  }
   state = { currentUser: null }
 
   componentDidMount() {
     const { currentUser } = firebase.auth();
     this.setState({ currentUser });
+  }
+
+  handleLogOut() {
+    firebase.auth().signOut()
+      .then(() => {
+        this.props.navigation.navigate('Loading');
+      })
+      .catch(err => {
+        console.error(err);
+      });
   }
 
   render() {
@@ -17,6 +31,7 @@ export default class Dashboard extends React.Component {
         <Text>
           Hi {currentUser && currentUser.email}!
         </Text>
+        <Button title='Log Out' onPress={this.handleLogOut}/>
       </View>
     );
   }
