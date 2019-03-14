e95a1737671c122eff451a55a2fc04453f97a97c
Steffeni Bunsick <steffenibunsick@gmail.com>
2019-03-14 17:17:24 -0500

    adds user data

--- screens/Dashboard.js
+++ screens/Dashboard.js
@@ -11,17 +11,20 @@ import {
 } from 'react-native';
 import firebase from '../server/config';
 import { Icon } from 'react-native-elements';
+import { connect } from 'react-redux';
+import { fetchUser } from '../store/user';
 
-export default class Dashboard extends React.Component {
+class Dashboard extends React.Component {
   constructor(props) {
     super(props);
     this.handleLogOut = this.handleLogOut.bind(this);
   }
   state = { currentUser: null };
 
-  componentDidMount() {
-    const { currentUser } = firebase.auth();
+  async componentDidMount() {
+    const { currentUser } = await firebase.auth();
     this.setState({ currentUser });
+    this.props.fetchUser(this.state.currentUser.uid);
   }
 
   handleLogOut() {
@@ -38,6 +41,12 @@ export default class Dashboard extends React.Component {
 
   render() {
     const { currentUser } = this.state;
+    if (this.props.user.places) {
+      //debugger;
+      console.log('USER', this.props.user.places.Singapore);
+    } else {
+      console.log('USER');
+    }
     return (
       // <View>
 
@@ -46,6 +55,9 @@ export default class Dashboard extends React.Component {
           <Icon reverse name="add" type="material" color="#ff9933" />
         </View>
         <View style={styles.container}>
+          {/* {this.props.user ? (
+            <Text>{this.props.user.user.places['Singapore']}</Text>
+          ) : null} */}
           <Button title="Log Out" onPress={this.handleLogOut} />
         </View>
       </ScrollView>
@@ -65,3 +77,20 @@ const styles = StyleSheet.create({
     alignItems: 'flex-end',
   },
 });
+
+const mapStateToProps = state => {
+  return {
+    user: state.user.user,
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchUser: userId => dispatch(fetchUser(userId)),
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Dashboard);
