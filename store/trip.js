9be0d500ffb2c181b4f35adf7fe456dee3622d84
Tori Pope <32719757+toricpope@users.noreply.github.com>
2019-03-14 14:35:56 -0500

    added provider to app.js (#21)

--- store/trip.js
+++ store/trip.js
@@ -1,11 +1,18 @@
-import axios from 'axios';
-
 // ACTION TYPES
 
 // INITIAL STATE
+const initialState = {
+  trips: []
+};
 
 // ACTION CREATORS
 
 // THUNK CREATORS
 
 // REDUCER
+export default (state= initialState, action) => {
+  switch(action.type) {
+    default:
+      return state;
+  }
+};
