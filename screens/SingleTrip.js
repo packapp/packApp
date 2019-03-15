11e09e42894f5337885560d2d714272d123fbc91
Steffeni Bunsick <steffenibunsick@gmail.com>
2019-03-15 14:51:47 -0500

    adds buttons for trips

--- screens/SingleTrip.js
+++ screens/SingleTrip.js
@@ -1,54 +1,60 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Text, View, Image, ScrollView, StyleSheet } from 'react-native';
-import {connect} from 'react-redux'
-import {fetchFlights} from '../store/flight'
+import { connect } from 'react-redux';
+import { fetchFlights } from '../store/flight';
 import { PricingCard, Tile } from 'react-native-elements';
 
 export class SingleTrip extends Component {
   componentDidMount() {
-    this.props.getFlights()
+    this.props.getFlights();
   }
   render() {
-
-    console.log('FLIGHTS', this.props.flights)
+    console.log('FLIGHTS', this.props.flights);
     return (
       <ScrollView>
-        <View style={{ flex: 1, justifyContent: "top", alignItems: "center" }}>
-        <Tile
-          imageSrc={{uri:'https://placeimg.com/400/100/nature'}}
-          title="Yosemite"
-          featured
-          caption="Dates.... "
-          height={200}
-        />
-        {this.props.flights.Carriers ? this.props.flights.Carriers.map(carrier => (
-          <PricingCard
-            containerStyle={{width: 400, height: 150}}
-            key={carrier.CarrierId}
-            color="#ff9933"
-            title={carrier.Name}
-            titleStyle={{fontSize: 18}}
-            // price="$0"
-            pricingStyle={{fontSize: 16}}
-            button={{ title: 'GET STARTED', icon: 'flight-takeoff'}}
+        <View style={{ flex: 1, justifyContent: 'top', alignItems: 'center' }}>
+          <Tile
+            imageSrc={{ uri: 'https://placeimg.com/400/100/nature' }}
+            title="Yosemite"
+            featured
+            caption="Dates.... "
+            height={200}
           />
-        )) : <Text>No flights</Text>}
+          {this.props.flights.Carriers ? (
+            this.props.flights.Carriers.map(carrier => (
+              <PricingCard
+                containerStyle={{ width: 400, height: 150 }}
+                key={carrier.CarrierId}
+                color="#ff9933"
+                title={carrier.Name}
+                titleStyle={{ fontSize: 18 }}
+                // price="$0"
+                pricingStyle={{ fontSize: 16 }}
+                button={{ title: 'GET STARTED', icon: 'flight-takeoff' }}
+              />
+            ))
+          ) : (
+            <Text>No flights</Text>
+          )}
         </View>
       </ScrollView>
-    )
+    );
   }
 }
 
 const mapState = state => {
   return {
-    flights: state.flight
-  }
-}
+    flights: state.flight,
+  };
+};
 
-const mapDispatch = (dispatch) => {
+const mapDispatch = dispatch => {
   return {
-    getFlights: () => dispatch(fetchFlights())
-  }
-}
+    getFlights: () => dispatch(fetchFlights()),
+  };
+};
 
-export default connect(mapState, mapDispatch)(SingleTrip)
+export default connect(
+  mapState,
+  mapDispatch
+)(SingleTrip);
