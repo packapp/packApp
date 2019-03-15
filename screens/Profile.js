048f789b85538579399456a7e8a02a5d81b16880
Tori Pope <32719757+toricpope@users.noreply.github.com>
2019-03-15 17:28:23 -0500

    made nav bar smaller (#34)

--- screens/Profile.js
+++ screens/Profile.js
@@ -49,7 +49,7 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     bottom: 0,
-    height: 100,
+    height: 70,
     backgroundColor: '#f8f8f8',
     flexDirection: 'row',
     alignItems: 'center',
