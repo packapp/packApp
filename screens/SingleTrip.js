c038ec18273b940b9a4f92947ad2f3d2cbc48937
Grant Weiss <grant.sweiss@gmail.com>
2019-03-15 12:09:01 -0500

    update flight info

--- screens/SingleTrip.js
+++ screens/SingleTrip.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, ScrollView, StyleSheet } from 'react-native';
 import {connect} from 'react-redux'
 import {fetchFlights} from '../store/flight'
+import { PricingCard, Tile } from 'react-native-elements';
 
 export class SingleTrip extends Component {
   componentDidMount() {
@@ -9,15 +10,31 @@ export class SingleTrip extends Component {
   }
   render() {
 
-    console.log(this.props)
+    console.log('FLIGHTS', this.props.flights)
     return (
-      <View style={{ flex: 1, justifyContent: "top", alignItems: "center" }}>
-        <Image source={{uri: 'https://placeimg.com/400/100/nature'}}
-       style={{width: 400, height: 200}} />
-       {this.props.flights ? this.props.flights.map(flight => (
-        <Text key={flight.CityId}>{flight.CityName}</Text>
-       )) : 'no flights'}
-      </View>
+      <ScrollView>
+        <View style={{ flex: 1, justifyContent: "top", alignItems: "center" }}>
+        <Tile
+          imageSrc={{uri:'https://placeimg.com/400/100/nature'}}
+          title="Yosemite"
+          featured
+          caption="Dates.... "
+          height={200}
+        />
+        {this.props.flights.Carriers ? this.props.flights.Carriers.map(carrier => (
+          <PricingCard
+            containerStyle={{width: 400, height: 150}}
+            key={carrier.CarrierId}
+            color="#ff9933"
+            title={carrier.Name}
+            titleStyle={{fontSize: 18}}
+            // price="$0"
+            pricingStyle={{fontSize: 16}}
+            button={{ title: 'GET STARTED', icon: 'flight-takeoff'}}
+          />
+        )) : <Text>No flights</Text>}
+        </View>
+      </ScrollView>
     )
   }
 }
